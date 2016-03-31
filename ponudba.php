7edda5abf01893ae737a7955a9a04dc7076fa069
Nejc von Podkraj <nejko.savi@gmail.com>
2016-03-31 13:27:27 +0200

    Ponudba update
    Sam ponudba €, drugi podatki iz profila

--- ponudba.php
+++ ponudba.php
@@ -4,12 +4,9 @@ include_once 'database.php';
 ?>
 
 <h1>Ponudba</h1>
-<form action="demo_form.asp">
-  Ime<input type="text" name="im"><br>
-  Priimek<input type="text" name="pr"><br>
-  e-mail<input type="text" name="em"><br>
-  Naslov<input type="text" name="ns"><br>
-  Ponudba<input type="text" name="pn"><br>
+<form>
+ 
+  Ponudba: <input type="text" name="pn">€<br>
   <input type="submit" value="Pošlji">
 </form>
 
