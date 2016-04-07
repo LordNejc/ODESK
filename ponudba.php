d77cf3de8827422e509f5b3762dcf20c55485fe1
raikkonen06 <denis.habot@hotmail.com>
2016-04-07 11:34:25 +0200

    OdeskNajnovejše
    Odesk s sporočili

--- ponudba.php
+++ ponudba.php
@@ -1,39 +1,18 @@
- <?php
- $username = 'root';
-    $password = '';
-    $database = 'odesk';
-    $server = 'localhost';
-    //povezava na podatkovno bazo
-    $link = mysqli_connect($server, $username, $password, $database);
-    
-    //za pravilno delanje šumnikov
-    mysqli_query($link,"SET NAMES 'utf8'"); 
-	
-	// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-	
-?>
-
-
 <?php
 include_once 'header.php';
 include_once 'database.php';
 ?>
 
 <h1>Ponudba</h1>
-<form>
- <?php
- echo $pn="Ponudba: <input type=text name=pn>€<br>"
-  ?>
+<form action="demo_form.asp">
+  Ime<input type="text" name="im"><br>
+  Priimek<input type="text" name="pr"><br>
+  e-mail<input type="text" name="em"><br>
+  Naslov<input type="text" name="ns"><br>
+  Ponudba<input type="text" name="pn"><br>
   <input type="submit" value="Pošlji">
-  $query = "SELECT * FROM countries";
-  $result = mysqli_query($link, $query);
 </form>
 
 <?php
-$query = "UPDATE sporocila(cena) VALUE("$pn") ";
-mysqli_query($link, $query);
-
 include_once 'footer.php';
 ?>
